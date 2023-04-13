df9c733e73cb18f719e665a62e1b54764d172b25
Mosa Daradkah <daradmosa@gmail.com>
2023-04-14 00:14:47 +0300

    reset

--- server.js
+++ server.js
@@ -2,27 +2,6 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
   }
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBxVAZUGMjVghrJYYeO5JJkyzO14V0YREw",
-  authDomain: "mata3em-dc8b2.firebaseapp.com",
-  projectId: "mata3em-dc8b2",
-  storageBucket: "mata3em-dc8b2.appspot.com",
-  messagingSenderId: "78446309158",
-  appId: "1:78446309158:web:196f761c0503802eb09be7",
-  measurementId: "G-FEB9FT0EF5"
-};
-
-// Initialize Firebase
-const fire = initializeApp(firebaseConfig);
-const analytics = getAnalytics(fire);
 
 const express = require("express")
 const app = express()
